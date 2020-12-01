30afb9096489cb73aaf8cd2748ccf574bb6e6e44
Chieh Wang <chiehwang1226@gmail.com>
2020-11-30 21:04:10 -0700

    added manage page tabs

--- src/components/recipe-row/RecipeRow.jsx
+++ src/components/recipe-row/RecipeRow.jsx
@@ -4,13 +4,17 @@ import './RecipeRow.styles.scss';
 import { IconButton } from "@material-ui/core";
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Switch from '@material-ui/core/Switch';
 
 const RecipeRow = ({ userRecipe }) => {
   const { title, createdAt, updatedAt } = userRecipe;
   return (
     <div className='recipe-row-container'>
       <span className='recipe-row-title'>{title}</span>
-      <span className='recipe-row-public'>{userRecipe.public ? 'true' : 'false'}</span>
+      <div className='recipe-row-public'>
+        <Switch checked={userRecipe.public}
+          inputProps={{ 'aria-label': 'secondary checkbox' }}/>
+      </div>
       <span className='recipe-row-createdat'>{createdAt.substring(0,10)}</span>
       <span className='recipe-row-updatedat'>{updatedAt.substring(0,10)}</span>
       <div className='recipe-row-edit'>
--- src/containers/user-manage-page/UserManagePage.jsx
+++ src/containers/user-manage-page/UserManagePage.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './UserManagePage.styles.scss';
 
 import PersonalInfo from '../../components/personal-info/PersonalInfo';
 import RecipeTable from '../../components/recipe-table/RecipeTable';
 
-const UserManagePage = () => {
-  return (
-    <div className='user-manage-page'>
-      <div className='user-manage-page-main'>
-        <RecipeTable />
+class UserManagePage extends Component {
+  handleClick = event => {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className='user-manage-page'>
+        <div className='user-manage-tab'>
+          <button style={{marginRight:"40px"}} className="manage-btn" value='editProfile' onClick={this.handleClick}>Edit Profile</button>
+          <button className="manage-btn" value='recipeManage' onClick={this.handleClick}>Recipe Overview</button>
+        </div>
+        <div className='user-manage-page-recipe'>
+          <RecipeTable />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default UserManagePage;
