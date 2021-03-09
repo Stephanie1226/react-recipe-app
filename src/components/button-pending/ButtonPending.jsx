1c692d4f70b1c3f993aafc6dc184e5fe4c724d4a
Chieh Wang <chiehwang1226@gmail.com>
2021-03-08 18:45:39 -0700

    test 1

--- src/components/button-pending/ButtonPending.jsx
+++ src/components/button-pending/ButtonPending.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ButtonPending = () => {
   return (
     <div>
+      {console.log("testtesttest")}
       <div className="spinner-grow text-secondary" role="status">
         <span className="sr-only">Loading...</span>
       </div>
